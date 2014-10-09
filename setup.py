224b1f24d6eabd6cfc8392ceb57acfa10e2e4225
Peter <peter.graf@nrel.gov>
2014-10-09 15:06:51 -0600

    no boost in rotorse

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ if platform.system() == 'Windows':
 		    package_dir={'': 'src/rotorse'}, 
 		    ext_modules=[Extension('_curvefem', ['src/rotorse/CurveFEMPy.f90'], 
 		        extra_compile_args=['-O2'], 
-		        include_dirs=['C:/boost_1_55_0'], 
-		        library_dirs=['C:/boost_1_55_0/stage/lib', 'C:/lapack'], 
-		        libraries=['boost_python-mgw46-mt-1_55', 'lapack'] 
+		        include_dirs=[''], 
+		        library_dirs=['C:/lapack'], 
+		        libraries=['lapack'] 
 		        )], 
 		) 
 elif platform.system() == 'Darwin':
@@ -50,5 +50,5 @@ else:
         name='curvefem',
         package_dir={'': 'src/rotorse'},
         ext_modules=[Extension('_curvefem', ['src/rotorse/CurveFEMPy.f90'], extra_compile_args=['-O2'],
-                               libraries=['boost_python', 'lapack'])])
+                               libraries=['lapack'])])
 
