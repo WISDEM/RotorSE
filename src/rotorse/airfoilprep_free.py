Fixed FD XFOIL gradients

src/rotorse/airfoilprep_free.py
@@ -829,7 +829,7 @@ class Airfoil(object):
                 cl = np.delete(cl, to_delete)
                 cd = np.delete(cd, to_delete)
                 alphas = np.delete(alphas, to_delete)
-                print alphas, cl, cd
+                print CST
                 polars.append(polarType(Re, alphas, cl, cd, cm))
 
             else:
@@ -1282,7 +1282,7 @@ class Airfoil(object):
             airfoil.mach = 0.0 # Uinf / 340.29
             airfoil.iter = 100
 
-            angle = alpha
+            angle = deepcopy(alpha)
             cl, cd, cm, lexitflag = airfoil.solveAlphaComplex(angle)
             if lexitflag:
                 cl = -10.0
@@ -1423,7 +1423,7 @@ class Airfoil(object):
                 dcl_dcst[i+4] = np.imag(cl_complex)/np.imag(cs_step)
                 dcd_dcst[i+4] = np.imag(cd_complex)/np.imag(cs_step)
         else:
-            step_size = 1e-2
+            step_size = 1e-6
             fd_step = step_size
             for i in range(len(wl)):
                 wl_fd1 = np.real(deepcopy(wl))
@@ -1431,7 +1431,9 @@ class Airfoil(object):
                 wl_fd1[i] -= fd_step
                 wl_fd2[i] += fd_step
                 cl_fd1, cd_fd1 = cstReal(alpha, Re, wl_fd1, np.real(wu), N, dz, Uinf=10.0)
+                cl_fd1, cd_fd1 = deepcopy(cl_fd1), deepcopy(cd_fd1)
                 cl_fd2, cd_fd2 = cstReal(alpha, Re, wl_fd2, np.real(wu), N, dz, Uinf=10.0)
+                cl_fd2, cd_fd2 = deepcopy(cl_fd2), deepcopy(cd_fd2)
                 dcl_dcst[i] = (cl_fd2 - cl_fd1)/(2.*fd_step)
                 dcd_dcst[i] = (cd_fd2 - cd_fd1)/(2.*fd_step)
                 wu_fd1 = np.real(deepcopy(wu))
@@ -1439,7 +1441,9 @@ class Airfoil(object):
                 wu_fd1[i] -= fd_step
                 wu_fd2[i] += fd_step
                 cl_fd1, cd_fd1 = cstReal(alpha, Re, np.real(wl), wu_fd1, N, dz, Uinf=10.0)
+                cl_fd1, cd_fd1 = deepcopy(cl_fd1), deepcopy(cd_fd1)
                 cl_fd2, cd_fd2 = cstReal(alpha, Re, np.real(wl), wu_fd2, N, dz, Uinf=10.0)
+                cl_fd2, cd_fd2 = deepcopy(cl_fd2), deepcopy(cd_fd2)
                 dcl_dcst[i+4] = (cl_fd2 - cl_fd1)/(2.*fd_step)
                 dcd_dcst[i+4] = (cd_fd2 - cd_fd1)/(2.*fd_step)
 
@@ -2286,7 +2290,7 @@ class CCAirfoil:
         alphas = np.insert(alphas, 4, 4.5)
         alphas = np.insert(alphas, 3, -2.)
         alphas = np.insert(alphas, 5, 2.)
-        Re = 1e7 #1e6
+        Re = 1e6
         af = Airfoil.initFromCST(CST, alphas, [Re], CFDorXFOIL, processors=processors, iterations=iterations)
         r_over_R = 0.5
         chord_over_r = 0.15
@@ -2354,7 +2358,7 @@ class CCAirfoil:
         else:
             cl, cd, dcl_dCST, dcd_dCST = cfdGradients(CST, alpha, Re, airfoil_analysis_options['iterations'], airfoil_analysis_options['processors'], airfoil_analysis_options['FDorCS'], Uinf, ComputeGradients, GenerateMESH=True)
 
-        return dcl_dCST, dcl_dCST
+        return dcl_dCST, dcd_dCST
 
 
 def cfdGradients(CST, alpha, Re, iterations, processors, FDorCS, Uinf, ComputeGradients, GenerateMESH=True):
@@ -2741,6 +2745,9 @@ def xfoilGradients(CST, alpha, Re, FDorCS):
         n1 = len(CST)/2
     except:
         n1 = 4
+    if n1 == 0:
+        n1 = 4
+        CST = CST[0]
     wu = np.zeros(n1, dtype=complex)
     wl = np.zeros(n1, dtype=complex)
     for j in range(n1):
@@ -2758,9 +2765,6 @@ def xfoilGradients(CST, alpha, Re, FDorCS):
         wu = higher
     N = 120
     dz = 0.
-
-
-
     dcl_dcst, dcd_dcst = np.zeros(8), np.zeros(8)
     cl, cd = cstReal(alpha, Re, np.real(wl), np.real(wu), N, dz, Uinf=10.0)
 
@@ -2780,7 +2784,7 @@ def xfoilGradients(CST, alpha, Re, FDorCS):
             dcl_dcst[i+4] = np.imag(cl_complex)/np.imag(cs_step)
             dcd_dcst[i+4] = np.imag(cd_complex)/np.imag(cs_step)
     else:
-        step_size = 1e-2
+        step_size = 1e-6
         fd_step = step_size
         for i in range(len(wl)):
             wl_fd1 = np.real(deepcopy(wl))
@@ -2788,7 +2792,9 @@ def xfoilGradients(CST, alpha, Re, FDorCS):
             wl_fd1[i] -= fd_step
             wl_fd2[i] += fd_step
             cl_fd1, cd_fd1 = cstReal(alpha, Re, wl_fd1, np.real(wu), N, dz, Uinf=10.0)
+            cl_fd1, cd_fd1 = deepcopy(cl_fd1), deepcopy(cd_fd1)
             cl_fd2, cd_fd2 = cstReal(alpha, Re, wl_fd2, np.real(wu), N, dz, Uinf=10.0)
+            cl_fd2, cd_fd2 = deepcopy(cl_fd2), deepcopy(cd_fd2)
             dcl_dcst[i] = (cl_fd2 - cl_fd1)/(2.*fd_step)
             dcd_dcst[i] = (cd_fd2 - cd_fd1)/(2.*fd_step)
             wu_fd1 = np.real(deepcopy(wu))
@@ -2796,7 +2802,9 @@ def xfoilGradients(CST, alpha, Re, FDorCS):
             wu_fd1[i] -= fd_step
             wu_fd2[i] += fd_step
             cl_fd1, cd_fd1 = cstReal(alpha, Re, np.real(wl), wu_fd1, N, dz, Uinf=10.0)
+            cl_fd1, cd_fd1 = deepcopy(cl_fd1), deepcopy(cd_fd1)
             cl_fd2, cd_fd2 = cstReal(alpha, Re, np.real(wl), wu_fd2, N, dz, Uinf=10.0)
+            cl_fd2, cd_fd2 = deepcopy(cl_fd2), deepcopy(cd_fd2)
             dcl_dcst[i+4] = (cl_fd2 - cl_fd1)/(2.*fd_step)
             dcd_dcst[i+4] = (cd_fd2 - cd_fd1)/(2.*fd_step)
 

src/rotorse/ccblade.py
@@ -292,13 +292,13 @@ class CCBlade:
         self.iterRe = iterRe
         self.derivatives = derivatives
 
-        if airfoil_parameterization is not None and airfoil_options is not None:
+        if airfoil_parameterization is not None and airfoil_options is not None and derivatives:
             self.airfoil_parameterization = airfoil_parameterization
             self.airfoil_analysis_options = airfoil_options
             self.freeform = True
         else:
             self.freeform = False
-        self.freeform = False
+        # self.freeform = False
         # check if no precurve / presweep
         if precurve is None:
             precurve = np.zeros(len(r))
@@ -387,7 +387,7 @@ class CCBlade:
             dx_dx[0, :], dcl_dx, dcd_dx, dx_dx[3, :], dx_dx[4, :], **self.bemoptions)
 
 
-        if self.freeform:
+        if self.freeform and self.freeform_gradient:
             fzero_cl, dR_dcl, a, ap,  = _bem.coefficients_dv(r, chord, self.Rhub, self.Rtip,
                 phi, cl, 1, cd, 0, self.B, Vx, Vy, **self.bemoptions)
             fzero_cd, dR_dcd, a, ap,  = _bem.coefficients_dv(r, chord, self.Rhub, self.Rtip,
@@ -430,7 +430,7 @@ class CCBlade:
             return Np, Tp, 0.0, 0.0, 0.0
         # derivative of residual function
         if rotating:
-            if self.freeform:
+            if self.freeform and self.freeform_gradient:
                 dR_dx, da_dx, dap_dx, dR_dcst, dcl_dcst, dcd_dcst = self.__residualDerivatives(phi, r, chord, theta, af, Vx, Vy, airfoil_parameterization)
             else:
                 dR_dx, da_dx, dap_dx = self.__residualDerivatives(phi, r, chord, theta, af, Vx, Vy)
@@ -470,19 +470,15 @@ class CCBlade:
         dNp_dx = Np*(1.0/cn*dcn_dx + 2.0/W*dW_dx + 1.0/chord*dchord_dx)
         dTp_dx = Tp*(1.0/ct*dct_dx + 2.0/W*dW_dx + 1.0/chord*dchord_dx)
 
-        if self.freeform and self.derivatives:
-            dphi_dcst = 0.0
-            dchord_dcst = 0.0
-            dW_dcst = 0.0
+        if self.freeform and self.freeform_gradient:
+            dphi_dcst = dcl_dalpha**-1*dcl_dcst + dcd_dalpha**-1*dcd_dcst
             dcn_dcst = dcl_dcst*cphi - cl*sphi*dphi_dcst + dcd_dcst*sphi + cd*cphi*dphi_dcst
             dct_dcst = dcl_dcst*sphi + cl*cphi*dphi_dcst - dcd_dcst*cphi + cd*sphi*dphi_dcst
-            dNp_dcst = Np*(1.0/cn*dcn_dcst + 2.0/W*dW_dcst + 1.0/chord*dchord_dcst)
-            dTp_dcst = Tp*(1.0/ct*dct_dcst + 2.0/W*dW_dcst + 1.0/chord*dchord_dcst)
+            dNp_dcst = Np*(1.0/cn*dcn_dcst)
+            dTp_dcst = Tp*(1.0/ct*dct_dcst)
             return Np, Tp, dNp_dx, dTp_dx, dR_dx, dNp_dcst, dTp_dcst, dR_dcst
 
         return Np, Tp, dNp_dx, dTp_dx, dR_dx
-
-
 
 
     def __windComponents(self, Uinf, Omega, azimuth):
@@ -578,8 +574,8 @@ class CCBlade:
         dTp_dVy = np.zeros(n)
         dNp_dz = np.zeros((6, n))
         dTp_dz = np.zeros((6, n))
-        DNp_Dcst = np.zeros((n, 8))
-        DTp_Dcst = np.zeros((n, 8))
+        DNp_Dcst = np.zeros((6, 8))
+        DTp_Dcst = np.zeros((6, 8))
 
         errf = self.__errorFunction
         rotating = (Omega != 0)
@@ -622,8 +618,15 @@ class CCBlade:
                 # ----------------------------------------------------------------
 
             # derivatives of residual
-            if self.freeform and self.derivatives:
-                Np[i], Tp[i], dNp_dx, dTp_dx, dR_dx, dNp_dcst, dTp_dcst, dR_dcst = self.__loads(phi_star, rotating, *args, airfoil_parameterization=self.airfoil_parameterization[i])
+            af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
+            airfoils = [False, False, False, True, True, False, True, True, False, True, False, True, False, False, False, False, False]
+            if self.freeform:
+                if airfoils[i]:
+                    self.freeform_gradient = True
+                    Np[i], Tp[i], dNp_dx, dTp_dx, dR_dx, dNp_dcst, dTp_dcst, dR_dcst = self.__loads(phi_star, rotating, *args, airfoil_parameterization=self.airfoil_parameterization[af_idx[i-3]])
+                else:
+                    self.freeform_gradient = False
+                    Np[i], Tp[i], dNp_dx, dTp_dx, dR_dx = self.__loads(phi_star, rotating, *args)
             else:
                 Np[i], Tp[i], dNp_dx, dTp_dx, dR_dx = self.__loads(phi_star, rotating, *args)
 
@@ -641,9 +644,9 @@ class CCBlade:
                 DNp_Dx = dNp_dx - dNp_dy/dR_dy*dR_dx
                 DTp_Dx = dTp_dx - dTp_dy/dR_dy*dR_dx
 
-                if self.freeform:
-                    DNp_Dcst[i, :] = dNp_dcst - dNp_dy/dR_dy*dR_dcst
-                    DTp_Dcst[i, :] = dTp_dcst - dTp_dy/dR_dy*dR_dcst
+                if self.freeform and self.freeform_gradient:
+                    DNp_Dcst[af_idx[i-3], :] = dNp_dcst - dNp_dy/dR_dy*dR_dcst
+                    DTp_Dcst[af_idx[i-3], :] = dTp_dcst - dTp_dy/dR_dy*dR_dcst
 
                 # parse components
                 # z = [r, chord, theta, Rhub, Rtip, pitch]
@@ -734,13 +737,16 @@ class CCBlade:
             dTp['dpitch'] = dTp_dX[13, :].reshape(n, 1)
 
             if self.freeform:
-                dNp['dcst'] = np.zeros((17, 17*8))
-                dTp['dcst'] = np.zeros((17, 17*8))
-                for z in range(17):
-                    dNp_zeros = np.zeros((17,8))
-                    dTp_zeros = np.zeros((17,8))
-                    dNp_zeros[z, :] = DNp_Dcst[z]
-                    dTp_zeros[z, :] = DTp_Dcst[z]
+                dNp['dcst'] = np.zeros((n, 6*8))
+                dTp['dcst'] = np.zeros((n, 6*8))
+                k = 0
+                af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
+                for z in range(n):
+                    dNp_zeros = np.zeros((6,8))
+                    dTp_zeros = np.zeros((6,8))
+                    if z > 2:
+                        dNp_zeros[af_idx[z]-2, :] = DNp_Dcst[af_idx[z]-2]
+                        dTp_zeros[af_idx[z]-2, :] = DTp_Dcst[af_idx[z]-2]
                     dNp['dcst'][z] = dNp_zeros.flatten()
                     dTp['dcst'][z] = dTp_zeros.flatten()
 
@@ -825,8 +831,8 @@ class CCBlade:
             dQ_ds = np.zeros((npts, 11))
             dT_dv = np.zeros((npts, 5, len(self.r)))
             dQ_dv = np.zeros((npts, 5, len(self.r)))
-            dT_dcst = np.zeros((npts, len(self.r), 8))
-            dQ_dcst = np.zeros((npts, len(self.r), 8))
+            dT_dcst = np.zeros((npts, 6*8))
+            dQ_dcst = np.zeros((npts, 6*8))
 
         for i in range(npts):  # iterate across conditions
 
@@ -840,12 +846,12 @@ class CCBlade:
                 else:
 
                     Np, Tp, dNp, dTp = self.distributedAeroLoads(Uinf[i], Omega[i], pitch[i], azimuth)
-                    self.freeform = False
+
                     if self.freeform:
                         dT_ds_sub, dQ_ds_sub, dT_dv_sub, dQ_dv_sub, dT_dcst_sub, dQ_dcst_sub = self.__thrustTorqueDeriv(
                             Np, Tp, self._dNp_dX, self._dTp_dX, self._dNp_dprecurve, self._dTp_dprecurve, *args, dNp_dcst=dNp['dcst'], dTp_dcst=dTp['dcst'])
-                        dT_dcst[i, :, :] += self.B * dT_dcst_sub.T / nsec
-                        dQ_dcst[i, :, :] += self.B * dQ_dcst_sub.T / nsec
+                        dT_dcst[i, :] += self.B * dT_dcst_sub.T / nsec
+                        dQ_dcst[i, :] += self.B * dQ_dcst_sub.T / nsec
                     else:
                         dT_ds_sub, dQ_ds_sub, dT_dv_sub, dQ_dv_sub = self.__thrustTorqueDeriv(
                             Np, Tp, self._dNp_dX, self._dTp_dX, self._dNp_dprecurve, self._dTp_dprecurve, *args)

src/rotorse/main.py
@@ -158,22 +158,23 @@ basepath = '5MW_AFFiles' + os.path.sep
 # 0.7934264659 0.0548655175
 # 0.2826711866 0.0607062522
 # 0.517768895 0.1425374017
-# afinit = CCAirfoil.initFromAerodynFile
-# afinit2 = CCAirfoil.initFromCST  # just for shorthand
+afinit = CCAirfoil.initFromAerodynFile
+afinit2 = CCAirfoil.initFromCST  # just for shorthand
 # load all airfoils
-# airfoil_types = [0]*8
-# airfoil_types[0] = afinit(basepath + 'Cylinder1.dat')
-# airfoil_types[1] = afinit(basepath + 'Cylinder2.dat')
+airfoil_types = [0]*8
+airfoil_types[0] = afinit(basepath + 'Cylinder1.dat')
+airfoil_types[1] = afinit(basepath + 'Cylinder2.dat')
+airfoil_analysis_options = dict(AirfoilParameterization='CST', CFDorXFOIL='XFOIL', FDorCS='CS', iterations=20, processors=0, FreeFormDesign=True)
 
-# for i in range(len(airfoil_types)-2):
-#     airfoil_types[i+2] = afinit2(CST[i])
+for i in range(len(airfoil_types)-2):
+    airfoil_types[i+2] = afinit2(CST[i], airfoil_analysis_options['CFDorXFOIL'], airfoil_analysis_options['processors'], airfoil_analysis_options['iterations'])
 
 # place at appropriate radial stations
 af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
 
-# af = [0]*len(r)
-# for i in range(len(r)):
-#     af[i] = airfoil_types[af_idx[i]]
+af = [0]*17
+for i in range(17):
+    af[i] = airfoil_types[af_idx[i]]
 
 # CST_full_2 = np.zeros(len(CST_full))
 # for i in range(len(CST_full_2)):
@@ -184,14 +185,13 @@ af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
 #     for j in range(8):
 #         CST_full[i][j] = CST[af_idx[i]][0][j]
 # CST = CST_full.reshape(naero, 1, 8)
-airfoil_analysis_options = dict(AirfoilParameterization='CST', CFDorXFOIL='XFOIL', FDorCS='CS', iterations=20, processors=0)
 # airfoil_analysis_options = dict(AirfoilParameterization='CST', CFDorXFOIL='Files', FDorCS='CS', iterations=20, processors=0)
 # airfoil_analysis_options = dict(AirfoilParameterization='CST', CFDorXFOIL='CFD', FDorCS='CS', iterations=20000, processors=32)
 
 # rotor['rotor.airfoil_files'] = af  # (List): names of airfoil file
 rotor['rotor.airfoil_parameterization'] = np.array(CST)  # (List): names of airfoil file
 rotor['rotor.airfoil_analysis_options'] = airfoil_analysis_options  # (List): names of airfoil file
-rotor['rotor.airfoil_files'] = np.array(af2) # np.array(af)  # (List): names of airfoil file
+rotor['rotor.airfoil_files'] = np.array(af) # np.array(af)  # (List): names of airfoil file
 
 # ----------------------
 
@@ -322,15 +322,15 @@ print 'theta_sub =', rotor['theta_sub']
 print 'control:tsr =', rotor['control:tsr']
 
 # print rotor.root.list_connections()
-time0 = time.time()
-ww = rotor.calc_gradient(['r_max_chord', 'chord_sub', 'theta_sub', 'control:tsr'], ['obj'])
-print "Grad time", time.time() - time0
-
-test = open('Text.txt', 'w')
-testPartial = open('TextPartial.txt', 'w')
+# time0 = time.time()
+# ww = rotor.calc_gradient(['r_max_chord', 'chord_sub', 'theta_sub', 'control:tsr'], ['obj'])
+# print "Grad time", time.time() - time0
+#
+# test = open('Text.txt', 'w')
+# testPartial = open('TextPartial.txt', 'w')
 # partial= rotor.check_partial_derivatives(out_stream=testPartial)
 
-total = rotor.check_total_derivatives(out_stream=test, unknown_list=['AEP', 'obj'])
+# total = rotor.check_total_derivatives(out_stream=test, unknown_list=['AEP', 'obj'])
 
 
 # plt.figure()