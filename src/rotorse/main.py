Fix of gradients in incorrect location

src/rotorse/main.py
@@ -29,10 +29,10 @@ rotor.root = RotorSE(naero, nstr, npower)
 ### SETUP OPTIMIZATION
 # rotor.driver = pyOptSparseDriver()
 # rotor.driver.options['optimizer'] = 'SNOPT' #'SLSQP'
-# rotor.driver.add_desvar('r_max_chord', lower=0.1, upper=0.5)
-# rotor.driver.add_desvar('chord_sub', lower=1.3, upper=5.3)
-# rotor.driver.add_desvar('theta_sub', lower=-10.0, upper=30.0)
-# rotor.driver.add_desvar('control:tsr', lower=3.0, upper=14.0)
+rotor.driver.add_desvar('r_max_chord', lower=0.1, upper=0.5)
+rotor.driver.add_desvar('chord_sub', lower=1.3, upper=5.3)
+rotor.driver.add_desvar('theta_sub', lower=-10.0, upper=30.0)
+rotor.driver.add_desvar('control:tsr', lower=3.0, upper=14.0)
 lower = np.ones((6,8))*[[-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1],[-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1],[-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1],
                         [-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1],[-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1],[-0.6, -0.76, -0.4, -0.25, 0.13, 0.16, 0.13, 0.1]]
 upper = np.ones((6,8))*[[-0.13, -0.16, -0.13, 0.15, 0.55, 0.55, 0.4, 0.4],[-0.13, -0.16, -0.13, 0.15, 0.55, 0.55, 0.4, 0.4],[-0.13, -0.16, -0.13, 0.15, 0.55, 0.55, 0.4, 0.4],
@@ -50,10 +50,12 @@ rotor.driver.add_desvar('airfoil_parameterization', lower=lower, upper=upper)
 #rotor.driver.add_constraint('con7', upper=0.0)
 # rotor.driver.add_constraint('con_freeform', lower=0.05)
 # rotor.driver.add_constraint('concon', lower=1.0)
-rotor.driver.add_objective('obj')
-# rotor.driver.add_objective('AEP')
+# rotor.driver.add_objective('obj')
+#rotor.driver.add_objective('AEP')
 # rotor.driver.add_objective('mass_all_blades')
-# rotor.driver.add_objective('analysis.P')
+rotor.driver.add_objective('analysis.P')
+# rotor.driver.add_objective('powercurve.P')
+# rotor.driver.add_objective('dt.power')
 
 print "Setting up RotorSE..."
 rotor.setup(check=False)
@@ -87,7 +89,7 @@ rotor['nBlades'] = 3  # (Int): number of blades
 
 # === free form airfoil parameters ===
 airfoil_analysis_options = dict(AnalysisMethod='XFOIL', AirfoilParameterization='CST', GradientType='FD',
-                                CFDiterations=10000, CFDprocessors=0, FreeFormDesign=False, BEMSpline=True) ## airfoil_analysis_options: AnalysisMethod = {'Files', 'XFOIL', 'CFD'}, AirfoilParameterization={'None, 'CST', 'NACA'}, GradientType={'FD', 'CS'}
+                                CFDiterations=10000, CFDprocessors=0, FreeFormDesign=True, BEMSpline=True) ## airfoil_analysis_options: AnalysisMethod = {'Files', 'XFOIL', 'CFD'}, AirfoilParameterization={'None, 'CST', 'NACA'}, GradientType={'FD', 'CS'}
 # airfoil_analysis_options = dict(AnalysisMethod='CFD', AirfoilParameterization='CST', GradientType='FD', CFDiterations=7500, CFDprocessors=16, FreeFormDesign=True)
 af_idx = [0, 0, 1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7]
 af_str_idx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7]
@@ -291,14 +293,17 @@ print 'airfoil_parameterization = ', rotor['airfoil_parameterization']
 
 
 ## Gradient checks
-grad_total = open('total_gradient_check_coe_afp_withpowercurve4.txt', 'w')
+# grad_total = open('total_gradient_check_coe_afp_withpowercurve7.txt', 'w')
 # grad_partial = open('partial_gradient_check_coe2.txt', 'w')
-total = rotor.check_total_derivatives(out_stream=grad_total)
+# total = rotor.check_total_derivatives(out_stream=grad_total)
 # partial= rotor.check_partial_derivatives(out_stream=grad_partial)
-# grad = rotor.calc_gradient(['airfoil_parameterization'], ['obj'], mode='auto')
-# print "AD COE", grad
-# grad = rotor.calc_gradient(['airfoil_parameterization'], ['obj'], mode='fd')
-# print "FD", grad
+airfoil_analysis_options['FreeFormDesign'] = True
+grad = rotor.calc_gradient(['airfoil_parameterization'], ['obj'], mode='auto')
+print "AD COE", grad
+airfoil_analysis_options['FreeFormDesign'] = False
+rotor['airfoil_analysis_options'] = airfoil_analysis_options
+grad = rotor.calc_gradient(['airfoil_parameterization'], ['obj'], mode='fd')
+print "FD", grad
 # grad_partial.close()
 
 

src/rotorse/rotoraerodefaults.py
@@ -432,26 +432,23 @@ class CCBlade(Component):
         self.Omega_load = params['Omega_load']
         self.pitch_load = params['pitch_load']
         self.azimuth_load = params['azimuth_load']
-        if self.airfoil_analysis_options['AnalysisMethod'] != 'Files':
-            self.ccblade = CCBlade_PY(self.r, self.chord, self.theta, self.af, self.Rhub, self.Rtip, self.B,
-                self.rho, self.mu, self.precone, self.tilt, self.yaw, self.shearExp, self.hubHt,
-                self.nSector, self.precurve, self.precurveTip, tiploss=self.tiploss, hubloss=self.hubloss,
-                wakerotation=self.wakerotation, usecd=self.usecd, derivatives=False, airfoil_parameterization=self.airfoil_parameterization, airfoil_options=self.airfoil_analysis_options)
+        if self.airfoil_analysis_options['FreeFormDesign'] and self.airfoil_analysis_options['AnalysisMethod'] != 'Files':
+            afp = self.airfoil_parameterization
         else:
-            self.ccblade = CCBlade_PY(self.r, self.chord, self.theta, self.af, self.Rhub, self.Rtip, self.B,
-                self.rho, self.mu, self.precone, self.tilt, self.yaw, self.shearExp, self.hubHt,
-                self.nSector, self.precurve, self.precurveTip, tiploss=self.tiploss, hubloss=self.hubloss,
-                wakerotation=self.wakerotation, usecd=self.usecd, derivatives=True, airfoil_parameterization=None, airfoil_options=self.airfoil_analysis_options)
-
+            afp = None
+        self.ccblade = CCBlade_PY(self.r, self.chord, self.theta, self.af, self.Rhub, self.Rtip, self.B,
+                    self.rho, self.mu, self.precone, self.tilt, self.yaw, self.shearExp, self.hubHt,
+                    self.nSector, self.precurve, self.precurveTip, tiploss=self.tiploss, hubloss=self.hubloss,
+                    wakerotation=self.wakerotation, usecd=self.usecd, derivatives=True, airfoil_parameterization=afp, airfoil_options=self.airfoil_analysis_options)
         if self.run_case == 'power':
             # power, thrust, torque
-            self.P, self.T, self.Q, = self.ccblade.evaluate(self.Uhub, self.Omega, self.pitch, coefficient=False)
+            self.P, self.T, self.Q, self.dP, self.dT, self.dQ = self.ccblade.evaluate(self.Uhub, self.Omega, self.pitch, coefficient=False)
             unknowns['T'] = self.T
             unknowns['Q'] = self.Q
             unknowns['P'] = self.P
         elif self.run_case == 'loads':
             # distributed loads
-            Np, Tp, = self.ccblade.distributedAeroLoads(self.V_load, self.Omega_load, self.pitch_load, self.azimuth_load)
+            Np, Tp, self.dNp, self.dTp = self.ccblade.distributedAeroLoads(self.V_load, self.Omega_load, self.pitch_load, self.azimuth_load)
 
             # concatenate loads at root/tip
             unknowns['loads:r'] = np.concatenate([[self.Rhub], self.r, [self.Rtip]])
@@ -487,27 +484,6 @@ class CCBlade(Component):
 
 
     def linearize(self, params, unknowns, resids):
-        if self.airfoil_analysis_options['FreeFormDesign']:
-            self.ccblade = CCBlade_PY(self.r, self.chord, self.theta, self.af, self.Rhub, self.Rtip, self.B,
-                    self.rho, self.mu, self.precone, self.tilt, self.yaw, self.shearExp, self.hubHt,
-                    self.nSector, self.precurve, self.precurveTip, tiploss=self.tiploss, hubloss=self.hubloss,
-                    wakerotation=self.wakerotation, usecd=self.usecd, derivatives=True, airfoil_parameterization=self.airfoil_parameterization, airfoil_options=self.airfoil_analysis_options)
-        else:
-            self.ccblade = CCBlade_PY(self.r, self.chord, self.theta, self.af, self.Rhub, self.Rtip, self.B,
-                    self.rho, self.mu, self.precone, self.tilt, self.yaw, self.shearExp, self.hubHt,
-                    self.nSector, self.precurve, self.precurveTip, tiploss=self.tiploss, hubloss=self.hubloss,
-                    wakerotation=self.wakerotation, usecd=self.usecd, derivatives=True, airfoil_parameterization=None, airfoil_options=self.airfoil_analysis_options)
-
-        if self.run_case == 'power':
-            # power, thrust, torque
-
-            self.P, self.T, self.Q, self.dP, self.dT, self.dQ \
-                = self.ccblade.evaluate(self.Uhub, self.Omega, self.pitch, coefficient=False)
-
-        elif self.run_case == 'loads':
-            # distributed loads
-            Np, Tp, self.dNp, self.dTp \
-                = self.ccblade.distributedAeroLoads(self.V_load, self.Omega_load, self.pitch_load, self.azimuth_load)
 
         J = {}
         if self.run_case == 'power':