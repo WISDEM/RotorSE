4b54d984865e65744106ba9c61e496fa1810d237
Bortolotti <pbortolo@nrel.gov>
2019-04-12 15:41:53 -0600

    BCM within RotorSE

--- src/rotorse/__init__.py
+++
@@ -1,5 +0,0 @@
-from math import pi
-from rotorse.rotor_geometry import TURBULENCE_CLASS, TURBINE_CLASS, DRIVETRAIN_TYPE
-# convert between rotations/minute and radians/second
-RPM2RS = pi/30.0
-RS2RPM = 30.0/pi
