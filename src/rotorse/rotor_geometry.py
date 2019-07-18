c03ece85eba66db18f2fc7d930cfdb653f352d42
Evan Gaertner <evan.gaertner@gmail.com>
2019-07-18 13:14:09 -0600

    bug fix to airfoil_position indepvar

--- src/rotorse/rotor_geometry.py
+++ src/rotorse/rotor_geometry.py
@@ -943,7 +943,7 @@ class RotorGeometry(Group):
         super(RotorGeometry, self).__init__()
         """rotor model"""
         NINPUT = len(RefBlade['ctrl_pts']['r_in'])
-        NAF    = len(refBlade['outer_shape_bem']['airfoil_position']['grid'])
+        NAF    = len(RefBlade['outer_shape_bem']['airfoil_position']['grid'])
 
         if flag_nd_opt == False:
             self.add('chord_in', IndepVarComp('chord_in', np.zeros(NINPUT), units='m'),   promotes=['*'])
